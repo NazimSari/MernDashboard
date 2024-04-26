32eaf164e9a47f3b45e6eb969d431064d87d208a
Nazim <yasarnazimsari@gmail.com>
2024-04-26 12:57:50 +0300

    create transactions component

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Transaction from "./models/Transaction.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 
 // Configuration
 dotenv.config();
@@ -44,6 +50,7 @@ mongoose
       console.log(`Server started on port:${PORT}`);
       // Product.insertMany(dataProduct);
       // ProductStat.insertMany(dataProductStat);
+      // Transaction.insertMany(dataTransaction);
       // User.insertMany(dataUser);
     });
     console.log("MongoDB Connected");
